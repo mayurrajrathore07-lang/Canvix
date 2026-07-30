76e52c0f6ff779b70db6c6376245fa1a6117d9ca
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-07-30 16:37:16 +0530

    feat: create AboutUs page and associated component scaffolds

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AboutHero from "../components/aboutus/AboutHero.jsx";
-import About from "../components/home/About.jsx";
+import AboutService from "../components/home/AboutService.jsx";
 import AboutTeam from "../components/aboutus/AboutTeam.jsx";
 import ProjectLetsTalk from "../components/project/project-lets-talk.jsx";
 
