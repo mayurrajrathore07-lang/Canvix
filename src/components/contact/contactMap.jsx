6beff6009936488cf964d40a43028305192fe4f3
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-07-30 17:21:40 +0530

    add

---
+++ src/components/contact/contactMap.jsx
@@ -0,0 +1,15 @@
+function Map() {
+    return (
+        <div className="w-full max-w-4xl h-96 mx-auto rounded-xl overflow-hidden shadow-lg">
+            <iframe
+                title="Google Map"
+                src="https://www.google.com/maps?q=Ahmedabad&output=embed"
+                className="w-full h-full"
+                loading="lazy"
+                allowFullScreen
+            />
+        </div>
+    );
+}
+
+export default Map;
--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Contact from "../components/contact/contact.jsx";
+import Map from "../components/contact/contactMap.jsx";
+
 
 export default function Contactpage() {
     return (
         <main className="bg-black text-white">
             <Contact />
+            <Map />
         </main>
     );
 }
