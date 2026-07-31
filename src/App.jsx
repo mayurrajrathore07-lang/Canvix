54989ac3d9d818bd048e7a653e1d04161e6a0406
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-07-31 17:53:49 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <div className="bg-white text-black min-h-screen flex flex-col justify-between selection:bg-black selection:text-white">
+      <div className="bg-white text-black min-h-screen flex flex-col justify-between selection:bg-black selection:text-white ml-14 mr-14">
         <Navbar />
         <div className="flex-grow">
           <Routes>
