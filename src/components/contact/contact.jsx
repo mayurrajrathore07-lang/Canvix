af80245ba7932e4259cc31f849776b48d4985085
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-08-05 11:11:46 +0530

    add

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -111,8 +111,6 @@ function Contact() {
                 </div>
 
 
-
-
                 <div className="bg-zinc-950 border border-zinc-800 rounded-3xl p-8 sm:p-12 shadow-2xl">
                     <h2 className="text-2xl font-bold text-white mb-6">Send Us a Message</h2>
 
