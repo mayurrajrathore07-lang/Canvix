ac1d376a97c2985faafbd236af0d6fc32e7569f2
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-08-05 15:27:52 +0530

    add

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -26,7 +26,8 @@ function Projects() {
             id: "4",
             image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80",
             title: "UI Design",
-            category: "Creative / Art direction / App",
+            category: "Creative / Art direction / App"
+
         },
     ];
 
