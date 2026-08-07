f42eb47bd5d3845ad7fac195c9e52eedc0be1a7b
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-08-07 12:43:30 +0530

    added

--- src/components/aboutus/AboutHero.jsx
+++ src/components/aboutus/AboutHero.jsx
@@ -16,7 +16,6 @@ const AboutHero = () => {
                         <h1 className="text-5xl md:text-7xl font-bold leading-tight">
                             Ready to take <br />
                             <span className="text-cyan-400">your business</span>
-                            <span className="text-yellow-400">our responsibility</span>
                             <br />
                             to the next level?
                         </h1>
