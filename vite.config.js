daaf77082eaeec19d65394d624a972e9ab74ed68
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-08-20 15:50:11 +0530

    fix: configure vite base for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Canvix/",
+
+  base: "/",
+
   server: {
     proxy: {
       "/api": {
